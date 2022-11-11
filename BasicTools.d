4a3267709db106b8e9a273ded69bbd9be0776130
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-11-12 00:34:13 +0900

    Update BasicTools.d

--- BasicTools.d
+++ BasicTools.d
@@ -43,9 +43,15 @@ bool IMPLY(bool a, bool b){ // noncommutative
 
 // basic functions
 
+bool[8] Int8Copy(bool[8] a){
+    
+    bool[8] b = a.dup;
+    return b;
+}
+
 bool[8] Int8Add(bool[8] a, bool[8] b){
     
-    bool k;
+    bool k = false;
     bool p;
     bool q;
     bool c;
@@ -63,12 +69,60 @@ bool[8] Int8Add(bool[8] a, bool[8] b){
     return sum;
 }
 
+bool[8] Int8Mul(bool[8] a, bool[8] b){
+    
+    bool k;
+    bool p;
+    bool q;
+    bool c;
+    bool[8] w = Int8Copy(a);
+    bool[8] mul = [0, 0, 0, 0, 0, 0, 0, 0];
+
+    foreach(i;0..8){
+        
+        if(b[i]){
+            
+            k = false; // initialized to false for every i
+
+            foreach(j;i..8){
+                
+    
+                q = XOR(w[j], mul[j]);
+                p = AND(w[j], mul[j]);
+                c = AND(k, q);
+                mul[j] = XOR(q, k);
+                k = OR(c, p); // XOR also works
+            }
+        }
+        
+        foreach_reverse(j;i+1..8){
+            w[j] = w[j-1];
+        }
+        
+        w[i]=0;
+    }
+
+    return mul;
+}
+
 // main
 
 void main(){
 
-    bool[8] a = [1,1,0,0,0,0,1,0];
-    bool[8] b = [1,1,0,1,0,1,0,1];
+    // examples
 
-    writeln(Int8Add(a, b));
+    bool[8] a = [1,0,1,0,0,0,0,0]; // 5
+    bool[8] b = [1,0,0,0,1,0,0,0]; // 17
+    writeln(Int8Add(a, b)); // 22
+    writeln(Int8Mul(a, b)); // 85
+
+    bool[8] c = [1,1,0,0,1,0,0,0]; // 19
+    bool[8] d = [0,1,1,0,0,0,0,0]; // 6
+    writeln(Int8Add(c, d)); // 25
+    writeln(Int8Mul(c, d)); // 114
+
+    bool[8] e = [1,0,1,0,0,1,0,0]; // 37
+    bool[8] f = [0,1,0,1,0,0,0,0]; // 10
+    writeln(Int8Add(e, f)); // 47
+    writeln(Int8Mul(e, f)); // 370 % 256 = 114
 }
